09ede0d2a1977b8b95946dbae0d6bd4c6cddd7de
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-11-25 18:11:07 +0400

    Added classes of created fragments

---
+++ app/src/main/java/com/example/cowdetection/presentation/view/FragmentBase.kt
@@ -0,0 +1,19 @@
+package com.example.cowdetection.presentation.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.cowdetection.R
+
+class FragmentBase : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_base, container, false)
+    }
+}

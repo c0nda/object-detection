ae8377b78ada6eb9ea62b7604d1c78afaf3b1f34
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-12-01 13:33:45 +0400

    Created repositories

---
+++ app/src/main/java/com/example/cowdetection/data/repository/CameraRepositoryImpl.kt
@@ -0,0 +1,11 @@
+package com.example.cowdetection.data.repository
+
+import com.example.cowdetection.domain.repository.CameraRepository
+
+class CameraRepositoryImpl : CameraRepository {
+
+    override fun takePhoto() {
+        TODO("Not yet implemented")
+    }
+
+}
---
+++ app/src/main/java/com/example/cowdetection/data/repository/GalleryRepositoryImpl.kt
@@ -0,0 +1,11 @@
+package com.example.cowdetection.data.repository
+
+import com.example.cowdetection.domain.repository.GalleryRepository
+
+class GalleryRepositoryImpl : GalleryRepository {
+
+    override fun choosePhotoFromGallery() {
+        TODO("Not yet implemented")
+    }
+
+}

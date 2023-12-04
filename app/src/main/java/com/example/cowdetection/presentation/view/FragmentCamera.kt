25eb56e96ea2517bc67374a2ba25fd90061b5a39
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-12-04 19:38:19 +0400

    Added navigation

--- app/src/main/java/com/example/cowdetection/presentation/view/FragmentCamera.kt
+++ app/src/main/java/com/example/cowdetection/presentation/view/FragmentCamera.kt
@@ -5,9 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.example.cowdetection.R
+import com.example.cowdetection.di.DaggerMainScreenComponent
+import com.example.cowdetection.presentation.viewmodel.BaseViewModel
 
-class FragmentCamera: Fragment() {
+class FragmentCamera : Fragment() {
+
+    private val component by lazy { DaggerMainScreenComponent.create() }
+
+    private val baseViewModel by activityViewModels<BaseViewModel> { component.viewModelFactory() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,4 +25,19 @@ class FragmentCamera: Fragment() {
         return inflater.inflate(R.layout.fragment_camera, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        baseViewModel.currentImage.observe(viewLifecycleOwner) {
+            if (it != null) {
+                navigateToFragmentImage()
+            }
+
+        }
+    }
+
+    private fun navigateToFragmentImage() {
+        val action = FragmentCameraDirections.actionFragmentCameraToFragmentImage()
+        findNavController().navigate(action)
+    }
 }

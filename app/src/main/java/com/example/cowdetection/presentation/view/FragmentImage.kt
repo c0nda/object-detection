3e86f80af71ea6acf96cdfdb13f12d3a9d46cd66
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-12-04 19:38:04 +0400

    Added subscription on photo changes

--- app/src/main/java/com/example/cowdetection/presentation/view/FragmentImage.kt
+++ app/src/main/java/com/example/cowdetection/presentation/view/FragmentImage.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import com.example.cowdetection.R
+import com.example.cowdetection.di.DaggerMainScreenComponent
+import com.example.cowdetection.presentation.viewmodel.BaseViewModel
 
-class FragmentImage: Fragment() {
+class FragmentImage : Fragment() {
+
+    private val component by lazy { DaggerMainScreenComponent.create() }
+
+    private val baseViewModel by activityViewModels<BaseViewModel> { component.viewModelFactory() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +23,15 @@ class FragmentImage: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_image, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val photo = view.findViewById<ImageView>(R.id.image)
+
+        baseViewModel.currentImage.observe(viewLifecycleOwner) {
+            photo.setImageURI(baseViewModel.currentImage.value)
+        }
     }
 }

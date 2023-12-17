bf99294b7cae55ea491100184272a2d1cc5dd53b
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-12-17 20:58:38 +0400

    New dependencies added to builder

--- app/src/main/java/com/example/cowdetection/di/MainScreenComponent.kt
+++ app/src/main/java/com/example/cowdetection/di/MainScreenComponent.kt
@@ -3,7 +3,10 @@ package com.example.cowdetection.di
 import androidx.lifecycle.ViewModel
 import com.example.cowdetection.presentation.viewmodel.BaseViewModel
 import com.example.cowdetection.presentation.viewmodel.BaseViewModelFactory
+import com.example.cowdetection.utils.filepath.FilePathProvider
+import com.example.cowdetection.utils.imageanalyzer.ImageAnalyzer
 import dagger.Binds
+import dagger.BindsInstance
 import dagger.Component
 import dagger.Module
 import dagger.multibindings.IntoMap
@@ -13,6 +16,18 @@ import dagger.multibindings.IntoMap
 interface MainScreenComponent {
 
     fun viewModelFactory(): BaseViewModelFactory
+
+    @Component.Builder
+    interface Builder {
+
+        @BindsInstance
+        fun filePath(filePathProvider: FilePathProvider): Builder
+
+        @BindsInstance
+        fun imageAnalyzer(imageAnalyzer: ImageAnalyzer): Builder
+
+        fun build(): MainScreenComponent
+    }
 }
 
 @Module

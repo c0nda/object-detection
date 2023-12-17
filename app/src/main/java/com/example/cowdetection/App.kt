2211df9bc10770fbd4f632329961075976804352
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-12-17 20:54:36 +0400

    AppComponent moved to object DI

--- app/src/main/java/com/example/cowdetection/App.kt
+++ app/src/main/java/com/example/cowdetection/App.kt
@@ -1,16 +1,17 @@
 package com.example.cowdetection
 
 import android.app.Application
-import com.example.cowdetection.di.AppComponent
 import com.example.cowdetection.di.DaggerAppComponent
 
 class App : Application() {
 
-    lateinit var appComponent: AppComponent
-
     override fun onCreate() {
         super.onCreate()
-        appComponent = DaggerAppComponent
+        initDI()
+    }
+
+    private fun initDI() {
+        DI.appComponent = DaggerAppComponent
             .builder()
             .appContext(this)
             .build()
---
+++ app/src/main/java/com/example/cowdetection/DI.kt
@@ -0,0 +1,9 @@
+package com.example.cowdetection
+
+import com.example.cowdetection.di.AppComponent
+
+object DI {
+
+    lateinit var appComponent: AppComponent
+        internal set
+}

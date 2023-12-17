54d3aaacb1639c4af2c25e429e5d0b58ea03eab8
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-12-17 20:54:10 +0400

    Removed takePhotoUseCase

--- app/src/main/java/com/example/cowdetection/di/UseCaseModule.kt
+++ app/src/main/java/com/example/cowdetection/di/UseCaseModule.kt
@@ -1,15 +1,11 @@
 package com.example.cowdetection.di
 
-import com.example.cowdetection.domain.usecase.ChoosePhotoFromGalleryUseCase
 import com.example.cowdetection.domain.usecase.TakePhotoUseCase
 import dagger.Module
 import dagger.Provides
 
 @Module
 class UseCaseModule {
-
-    @Provides
-    fun provideChoosePhotoFromGalleryUseCase() = ChoosePhotoFromGalleryUseCase()
 
     @Provides
     fun provideTakePhotoUseCase() = TakePhotoUseCase()

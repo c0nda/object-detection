917aa511204d75dae9a8206bedd16513844fbeab
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2023-12-17 20:56:20 +0400

    Added LiveData for imageCapture. Created functions for saving imageCapture and providing output directory

--- app/src/main/java/com/example/cowdetection/presentation/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/example/cowdetection/presentation/viewmodel/BaseViewModel.kt
@@ -1,22 +1,35 @@
 package com.example.cowdetection.presentation.viewmodel
 
 import android.net.Uri
+import androidx.camera.core.ImageCapture
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.cowdetection.domain.usecase.ChoosePhotoFromGalleryUseCase
 import com.example.cowdetection.domain.usecase.TakePhotoUseCase
+import com.example.cowdetection.utils.filepath.FilePathProvider
+import java.io.File
 import javax.inject.Inject
 
 class BaseViewModel @Inject constructor(
-    private val choosePhotoFromGalleryUseCase: ChoosePhotoFromGalleryUseCase,
-    private val takePhotoUseCase: TakePhotoUseCase
+    private val takePhotoUseCase: TakePhotoUseCase,
+    private val filePathProvider: FilePathProvider
 ) : ViewModel() {
 
     private val _currentImage = MutableLiveData<Uri?>(null)
     val currentImage: LiveData<Uri?> = _currentImage
 
-    fun save(imageUri: Uri?) {
+    private val _imageCapture = MutableLiveData<ImageCapture>()
+    val imageCapture: LiveData<ImageCapture> = _imageCapture
+
+    fun saveImageUri(imageUri: Uri?) {
         _currentImage.value = imageUri
+    }
+
+    fun saveImageCapture(imageCapture: ImageCapture) {
+        _imageCapture.value = imageCapture
+    }
+
+    fun getOutputDirectory(): File {
+        return filePathProvider.ioDirectoryPath()
     }
 }

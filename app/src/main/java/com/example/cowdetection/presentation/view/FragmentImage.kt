bbe5f591906746fd4c6153478fc4195e5d0657f5
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-01-10 10:45:26 +0400

    Added getting bitmap from Uri and analyzing bitmap

--- app/src/main/java/com/example/cowdetection/presentation/view/FragmentImage.kt
+++ app/src/main/java/com/example/cowdetection/presentation/view/FragmentImage.kt
@@ -1,17 +1,22 @@
 package com.example.cowdetection.presentation.view
 
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.cowdetection.DI
 import com.example.cowdetection.R
 import com.example.cowdetection.di.DaggerMainScreenComponent
 import com.example.cowdetection.presentation.viewmodel.BaseViewModel
+import kotlinx.coroutines.launch
 
 class FragmentImage : Fragment() {
 
@@ -36,12 +41,25 @@ class FragmentImage : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         val photo = view.findViewById<ImageView>(R.id.image)
+        val information = view.findViewById<TextView>(R.id.ivInformation)
 
         baseViewModel.currentImage.observe(viewLifecycleOwner) {
             if (it == null) {
                 navigateToFragmentCamera()
             } else {
                 photo.setImageURI(baseViewModel.currentImage.value)
+                val bitmap = MediaStore.Images.Media.getBitmap(
+                    requireContext().contentResolver,
+                    baseViewModel.currentImage.value
+                )
+                try {
+                    lifecycleScope.launch {
+                        val result = baseViewModel.analyzeBitmap(bitmap)
+                        information.text = result.toString()
+                    }
+                } catch (e: Exception) {
+                    Log.e("analyzer", e.printStackTrace().toString())
+                }
             }
         }
     }

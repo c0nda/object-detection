7b223e13d7f5fe6e60e8f58183bccff80c84b2b7
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-01-10 10:39:42 +0400

    Added init block. Created functions for getting image capture and analyzing bitmap

--- app/src/main/java/com/example/cowdetection/presentation/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/example/cowdetection/presentation/viewmodel/BaseViewModel.kt
@@ -1,18 +1,22 @@
 package com.example.cowdetection.presentation.viewmodel
 
+import android.graphics.Bitmap
 import android.net.Uri
+import android.util.Size
 import androidx.camera.core.ImageCapture
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.cowdetection.domain.usecase.TakePhotoUseCase
 import com.example.cowdetection.utils.filepath.FilePathProvider
+import com.example.cowdetection.utils.imageanalyzer.ImageAnalyzer
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import java.io.File
 import javax.inject.Inject
 
 class BaseViewModel @Inject constructor(
-    private val takePhotoUseCase: TakePhotoUseCase,
-    private val filePathProvider: FilePathProvider
+    private val filePathProvider: FilePathProvider,
+    private val imageAnalyzer: ImageAnalyzer
 ) : ViewModel() {
 
     private val _currentImage = MutableLiveData<Uri?>(null)
@@ -20,6 +24,12 @@ class BaseViewModel @Inject constructor(
 
     private val _imageCapture = MutableLiveData<ImageCapture>()
     val imageCapture: LiveData<ImageCapture> = _imageCapture
+
+    init {
+        _imageCapture.value = ImageCapture.Builder()
+            .setTargetResolution(Size(640, 640))
+            .build()
+    }
 
     fun saveImageUri(imageUri: Uri?) {
         _currentImage.value = imageUri
@@ -29,7 +39,17 @@ class BaseViewModel @Inject constructor(
         _imageCapture.value = imageCapture
     }
 
+    fun getImageCapture(): ImageCapture? {
+        return imageCapture.value
+    }
+
     fun getOutputDirectory(): File {
         return filePathProvider.ioDirectoryPath()
     }
+
+    suspend fun analyzeBitmap(bitmap: Bitmap): Float = coroutineScope {
+        val analysis = async { imageAnalyzer.analyzeImage(bitmap) }
+        return@coroutineScope analysis.await()
+    }
+
 }

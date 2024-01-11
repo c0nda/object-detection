65a4aa9ec7dee67167d6f2a835e23b5f8dadb061
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-01-11 19:45:05 +0400

    Changed logic of computing score

--- app/src/main/java/com/example/cowdetection/utils/prepostprocessor/PrePostProcessorImpl.kt
+++ app/src/main/java/com/example/cowdetection/utils/prepostprocessor/PrePostProcessorImpl.kt
@@ -10,8 +10,8 @@ class PrePostProcessorImpl @Inject constructor() : PrePostProcessor {
     companion object {
         const val outputRow = 25200
         const val outputColumn = 12
-        const val threshold = 0.8F
-        const val nmsLimit = 1
+        const val threshold = 0.85F
+        const val nmsLimit = 3
     }
 
     private fun nonMaxSuppression(
@@ -19,24 +19,22 @@ class PrePostProcessorImpl @Inject constructor() : PrePostProcessor {
         limit: Int,
         threshold: Float
     ): ArrayList<Result> {
-        boxes.sortWith { o1, o2 ->
-            o1.score.compareTo(o2.score)
-        }
-
+        val scores = boxes.sortedByDescending { it.score }
         val selected = ArrayList<Result>()
-        val active = BooleanArray(boxes.size) { true }
+        val active = BooleanArray(scores.size) { true }
         var numActive = active.size
 
         var done = false
-        for (i in 0 until boxes.size) {
-            if (!done && active[i]) {
-                val box1 = boxes[i]
+        for (i in scores.indices) {
+            if (done) break
+            if (active[i]) {
+                val box1 = scores[i]
                 selected.add(box1)
                 if (selected.size >= limit) break
 
-                for (j in i + 1 until boxes.size) {
+                for (j in i + 1 until scores.size) {
                     if (active[j]) {
-                        val box2 = boxes[j]
+                        val box2 = scores[j]
                         if (intersectionOverUnion(box1.rect, box2.rect) > threshold) {
                             active[j] = false
                             --numActive
@@ -70,7 +68,7 @@ class PrePostProcessorImpl @Inject constructor() : PrePostProcessor {
         val intersectionArea = max(intersectionMaxY - intersectionMinY, 0) *
                 max(intersectionMaxX - intersectionMinX, 0)
 
-        return (intersectionArea / (area1 + area2 - intersectionArea)).toFloat()
+        return intersectionArea.toFloat() / (area1 + area2 - intersectionArea)
     }
 
     override fun outputsToNMSPredictions(
@@ -84,7 +82,15 @@ class PrePostProcessorImpl @Inject constructor() : PrePostProcessor {
     ): ArrayList<Result> {
         val results = ArrayList<Result>()
         for (i in 0 until outputRow) {
-            if (outputs[i * outputColumn + 4] > threshold) {
+            var classIndex = 0
+            var score = outputs[i * outputColumn + 4]
+            for (j in 0 until outputColumn - 4) {
+                if (outputs[i * outputColumn + 4 + j] > score) {
+                    score = outputs[i * outputColumn + 4 + j]
+                    classIndex = j
+                }
+            }
+            if (score > threshold) {
                 val x = outputs[i * outputColumn]
                 val y = outputs[i * outputColumn + 1]
                 val w = outputs[i * outputColumn + 2]
@@ -95,29 +101,21 @@ class PrePostProcessorImpl @Inject constructor() : PrePostProcessor {
                 val right = imgScaleX * (x + w / 2)
                 val bottom = imgScaleY * (y + h / 2)
 
-                var max = outputs[i * outputColumn + 5]
-                var classIndex = 0
-                for (j in 0 until outputColumn - 5) {
-                    if (outputs[i * outputColumn + 5 + j] > max) {
-                        max = outputs[i * outputColumn + 5 + j]
-                        classIndex = j
-                    }
-                }
-
                 val rect = Rect(
                     (startX + ivScaleX * left).toInt(),
-                    (startY + top * ivScaleY).toInt(),
+                    (startY + ivScaleY * top).toInt(),
                     (startX + ivScaleX * right).toInt(),
                     (startY + ivScaleY * bottom).toInt()
                 )
+
                 val result = Result(
                     classIndex = classIndex,
-                    score = outputs[i * outputColumn + 4],
+                    score = score,
                     rect
                 )
                 results.add(result)
             }
         }
-        return nonMaxSuppression(boxes = results, limit = nmsLimit, threshold = threshold)
+        return nonMaxSuppression(boxes = results, limit = nmsLimit, threshold = 0.5F)
     }
 }

d01adffdcd88df48b2a12c79bfe71d6098ec422a
Georgiy Voroncov <egor.vorontsov03@mail.ru>
2024-01-17 20:31:54 +0400

    Changed classes on NN

--- app/src/main/java/com/example/cowdetection/presentation/ResultView.kt
+++ app/src/main/java/com/example/cowdetection/presentation/ResultView.kt
@@ -9,7 +9,9 @@ import android.graphics.Path
 import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
-import com.example.cowdetection.utils.prepostprocessor.AnalysisResult
+import com.example.cowdetection.utils.prepostprocessor.model.AnalysisResult
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class ResultView constructor(context: Context, attrs: AttributeSet?) : View(context, attrs) {
 
@@ -24,17 +26,17 @@ class ResultView constructor(context: Context, attrs: AttributeSet?) : View(cont
         const val TEXT_Y = 35
         const val TEXT_WIDTH = 260
         const val TEXT_HEIGHT = 50
-        val classes = listOf(
-            "coverall",
-            "gloves",
-            "helmet",
-            "mask_weared_incorrect",
-            "uniform",
-            "with_hat",
-            "with_mask",
-            "without_hat",
-            "without_mask"
-        )
+//        val classes = listOf(
+//            "coverall",
+//            "gloves",
+//            "helmet",
+//            "mask_weared_incorrect",
+//            "uniform",
+//            "with_hat",
+//            "with_mask",
+//            "without_hat",
+//            "without_mask"
+//        )
     }
 
     @SuppressLint("DrawAllocation")
@@ -42,7 +44,6 @@ class ResultView constructor(context: Context, attrs: AttributeSet?) : View(cont
         super.onDraw(canvas)
 
         if (results == null) return
-
         for (result in results!!.results) {
             paintRectangle.strokeWidth = 5F
             paintRectangle.style = Paint.Style.STROKE
@@ -54,6 +55,7 @@ class ResultView constructor(context: Context, attrs: AttributeSet?) : View(cont
                 (result.rect.left + TEXT_WIDTH).toFloat(),
                 (result.rect.top + TEXT_HEIGHT).toFloat()
             )
+
             path.addRect(rectF, Path.Direction.CW)
             paintText.color = Color.BLUE
             canvas.drawPath(path, paintText)
@@ -62,6 +64,18 @@ class ResultView constructor(context: Context, attrs: AttributeSet?) : View(cont
             paintText.strokeWidth = 0F
             paintText.style = Paint.Style.FILL
             paintText.textSize = 32F
+
+            val classes = ArrayList<String>()
+            val br = BufferedReader(InputStreamReader(context.assets.open("classes.txt")))
+            var line: String?
+            while (true) {
+                line = br.readLine()
+                if (line == null) {
+                    break
+                }
+                classes.add(line)
+            }
+
             canvas.drawText(
                 String.format("%s %.2f", classes[result.classIndex], result.score),
                 (result.rect.left + TEXT_X).toFloat(),
